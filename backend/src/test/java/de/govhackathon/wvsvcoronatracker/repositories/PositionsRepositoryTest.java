fd7bee9841010a17c44a0b08fb61c4837ff90299
Martin Reinhardt <contact@martinreinhardt-online.de>
2020-04-14 05:11:44 +0200

    chore(Refactoring): Restructe Unit Tests for Backend

--- backend/src/test/java/de/govhackathon/wvsvcoronatracker/repositories/PositionsRepositoryTest.java
+++ backend/src/test/java/de/govhackathon/wvsvcoronatracker/repositories/PositionsRepositoryTest.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.OffsetDateTime;
 import java.util.List;
